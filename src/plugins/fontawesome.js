07b4164ed66fd999c387a05d3141570405bdd967
Pedro León <pedro@wintercr.com>
2020-04-19 18:44:01 -0500

    adding static css for fontawesome icons

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,10 +1,12 @@
 import Vue from "vue"
 
-import { library } from "@fortawesome/fontawesome-svg-core"
+import { library, config } from "@fortawesome/fontawesome-svg-core"
 import { faEnvelope, faHeart } from "@fortawesome/free-solid-svg-icons"
 import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
+config.autoAddCss = false
 library.add(faEnvelope, faTwitter, faGithub, faHeart)
 
 const plugin = {
