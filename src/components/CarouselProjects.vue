3c772e0331d10007b5bfd6e50fb939449ac9d0d3
Pedro León <pedro@wintercr.com>
2024-11-02 00:04:23 -0500

    Avances con el carousel, falta contenido e imagenes

--- src/components/CarouselProjects.vue
+++ src/components/CarouselProjects.vue
@@ -6,22 +6,17 @@
     <div class="row mt-5 mb-0">
       <div class="col-md-3">
         <div class="nav flex-column nav-pills me-3 mb-2" role="tablist">
-          <button class="nav-link active" data-bs-toggle="pill" data-bs-target="#proyecto-1"
-            type="button"><font-awesome-icon icon="fa-solid fa-house" /> Viaja ya</button>
-          <button class="nav-link" data-bs-toggle="pill" data-bs-target="#proyecto-2" type="button">Blog</button>
-          <button class="nav-link" data-bs-toggle="pill" data-bs-target="#proyecto-3" type="button">Help</button>
+          <button v-for="(car, index) in favCarousel" class="nav-link" :class="{ 'active': index === 0 }"
+            :data-bs-target="`#proyecto-${index}`" data-bs-toggle="pill" type="button">
+            <font-awesome-icon :icon="car.iconNavBar" /> {{ car.titleNavBar }}
+          </button>
         </div>
       </div>
       <div class="col-md-9">
         <div class="tab-content">
-          <div class="tab-pane fade show active" id="proyecto-1" role="tabpanel" tabindex="0">
-            <CarouselForEach />
-          </div>
-          <div class="tab-pane fade" id="proyecto-2" role="tabpanel" tabindex="0">
-            <CarouselForEach />
-          </div>
-          <div class="tab-pane fade" id="proyecto-3" role="tabpanel" tabindex="0">
-            <CarouselForEach />
+          <div v-for="(car, index) in favCarousel" class="tab-pane fade" :class="{ 'active show': index === 0 }"
+            :id="`proyecto-${index}`" role="tabpanel" tabindex="0">
+            <CarouselForEach :car="car" />
           </div>
         </div>
       </div>
@@ -37,6 +32,47 @@ import CarouselForEach from './_CarouselForEach.vue';
 export default {
   components: {
     CarouselForEach
+  },
+  computed: {
+    favCarousel() {
+      return this.carousel.filter(x => x.isFavorite == true)
+    }
+  },
+  data() {
+    return {
+      carousel: [
+        {
+          id: 1,
+          title: "Viaja Ya",
+          titleNavBar: "Viaja Ya",
+          iconNavBar: "fa-solid fa-house",
+          description: "Viaja Ya es una agencia de viajes que se especializa en la venta de tours a través de contenido dinámico y consumo de APIs",
+          liveLink: "https://viajaya.wintercr.com",
+          linkCode: "https://github.com/LeonSolisPedro/ViajaYa",
+          images: [
+            "https://b2-backblaze-stackpath.b-cdn.net/1579919/jvezjw_b0e6664ff4d37dde447277873d0c7d5b59b17f06.jpg",
+            "https://as1.ftcdn.net/v2/jpg/08/16/30/22/1000_F_816302285_hckOpmwhWP0WeQzEi5qdYANf5w0HT1u2.jpg",
+            "https://www.surreal.co.za/leadership/swatches/swatch_landscape_superhero_jun2023_1_9b85.jpg"
+          ],
+          isFavorite: true
+        },
+        {
+          id: 2,
+          title: "BBB",
+          titleNavBar: "BB",
+          iconNavBar: "fa-solid fa-house",
+          description: "bbbbbbbb",
+          liveLink: "https://bbb.wintercr.com",
+          linkCode: "https://github.com/LeonSolisPedro/BBB",
+          images: [
+            "https://imgcdn.stablediffusionweb.com/2024/9/25/c61e95ee-c9ba-4541-a24d-8ec57e240d28.jpg",
+            "https://picsum.photos/536/354",
+            "https://picsum.photos/536/354"
+          ],
+          isFavorite: true
+        }
+      ]
+    }
   }
 }
 </script>
--- src/components/_CarouselForEach.vue
+++ src/components/_CarouselForEach.vue
@@ -1,39 +1,35 @@
 <template>
-  <div id="carouselExampleCaptions" class="carousel carousel-fade slide">
+  <div :id="`carouselExampleCaptions${car.id}`" class="carousel carousel-fade slide">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></button>
+      <button v-for="(img, index) in car.images" type="button" :data-bs-target="`#carouselExampleCaptions${car.id}`"
+        :data-bs-slide-to="index" :class="{ 'active': index === 0 }"></button>
     </div>
     <div class="carousel-inner rounded">
-      <div class="carousel-item active">
-        <img src="https://picsum.photos/536/354" class="d-block w-100 fantastic-image" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://picsum.photos/536/354" class="d-block w-100 fantastic-image" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="https://picsum.photos/536/354" class="d-block w-100 fantastic-image" alt="...">
+      <div v-for="(img, index) in car.images" class="carousel-item" :class="{ 'active': index === 0 }">
+        <img :src="img" class="d-block w-100 fantastic-image" alt="...">
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" :data-bs-target="`#carouselExampleCaptions${car.id}`"
+      data-bs-slide="prev">
       <span class="carousel-control-prev-icon"></span>
-      <span class="visually-hidden">Previous</span>
+      <span class="visually-hidden">Anterior</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" :data-bs-target="`#carouselExampleCaptions${car.id}`"
+      data-bs-slide="next">
       <span class="carousel-control-next-icon"></span>
-      <span class="visually-hidden">Next</span>
+      <span class="visually-hidden">Siguiente</span>
     </button>
   </div>
   <div class="mt-3">
-    <h4 class="text-center">Viaja ya</h4>
-    <p class="text-center">Viaja Ya es una agencia de viajes que se especializa en la venta de tours a través de contenido dinámico y consumo de APIs</p>
+    <h4 class="text-center">{{ car.title }}</h4>
+    <p class="text-center">{{ car.description }}</p>
   </div>
   <div class="text-center d-flex flex-column align-items-center gap-2 mt-3">
-    <a href="https://viajaya.wintercr.com" target="_blank" class="btn btn-primary btn-sm">Ver <font-awesome-icon class="ms-1" icon="fa-solid fa-play" /></a>
-    <a href="https://github.com/LeonSolisPedro/ViajaYa" target="_blank" class="btn btn-primary btn-sm">Ver código <font-awesome-icon class="ms-1" icon="fa-solid fa-code" /></a>
+    <a :href="car.liveLink" target="_blank" class="btn btn-primary btn-sm">Ver <font-awesome-icon class="ms-1"
+        icon="fa-solid fa-play" /></a>
+    <a :href="car.linkCode" target="_blank" class="btn btn-primary btn-sm">Ver código <font-awesome-icon class="ms-1"
+        icon="fa-solid fa-code" /></a>
   </div>
-  
 </template>
 
 
@@ -48,3 +44,15 @@
   object-fit: cover;
 }
 </style>
+
+
+<script>
+export default {
+  props: {
+    car: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
